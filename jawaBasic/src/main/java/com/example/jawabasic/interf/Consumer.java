33f679a57e1751b0333cb92e9678af2aa919499e
aizp <aizhiping@htang.com.cn>
2022-08-01 20:30:13 +0800

    生产者，消费者 PECS  原则。

--- jawaBasic/src/main/java/com/example/jawabasic/interf/Consumer.java
+++ jawaBasic/src/main/java/com/example/jawabasic/interf/Consumer.java
@@ -1,18 +1,52 @@
 package com.example.jawabasic.interf;
 
 
-public interface Consumer<T>{
-    void accept(T t) ;
+public interface Consumer<T> {
+    void accept(T t);
 }
-interface Provider<T>{
-    T provider() ;
+
+interface Provider<T> {
+    T provider();
 }
- class Test{
-     public static void main(String[] args) {
-         Consumer<? super String> consumer = System.out::println;
 
-         Provider<? extends String> provider = () -> "99";
+class Animal {
+    public String name;
 
-         consumer.accept(provider.provider());
-     }
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    protected void eat(String name) {
+        System.out.println(name.concat("eat"));
+    }
+}
+
+class Dog extends Animal {
+
+    public Dog() {
+        super("dog");
+    }
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void eat(String name) {
+        super.eat(name);
+        System.out.println("bone");
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Consumer<? super Animal> consumer = animal -> {animal.eat(animal.name);};
+
+        Provider<? extends Animal> provider = Dog::new;
+
+        Provider<? extends Animal> provider2 = () -> new Animal("an");
+
+        consumer.accept(provider.provider());
+        consumer.accept(provider2.provider());
+    }
 }
