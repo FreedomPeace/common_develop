f1914b7c32cbb8c5e49278f8d0a5bfda5df12b73
aizp <aizhiping@htang.com.cn>
2022-08-13 12:05:57 +0800

    recyclerview 画廊效果效果

--- common_app/common_function/src/main/java/cn/com/codequality/enter/EnterListActivity.java
+++ common_app/common_function/src/main/java/cn/com/codequality/enter/EnterListActivity.java
@@ -5,9 +5,16 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.lifecycle.MutableLiveData;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,12 +28,15 @@ import cn.com.codequality.R;
 
 public class EnterListActivity extends BaseActivity {
 
+    MutableLiveData<Integer> index  = new MutableLiveData<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_list);
         List<String> data = initData();
         initView(data);
+        setSwipeBackEnable(false);
     }
 
     private List<String> initData() {
@@ -35,7 +45,9 @@ public class EnterListActivity extends BaseActivity {
             PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_ACTIVITIES);
             ActivityInfo[] activities = packageInfo.activities;
             for (ActivityInfo activity : activities) {
-                data.add(activity.name);
+                String name = activity.name;
+                String simpleName = name.substring(name.lastIndexOf(".") - 1);
+                data.add(name);
             }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
@@ -45,9 +57,11 @@ public class EnterListActivity extends BaseActivity {
 
     private void initView(final List<String> data) {
         RecyclerView activityList = findViewById(R.id.activity_list);
-        activityList.setLayoutManager(new LinearLayoutManager(this));
-        activityList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration
-                .HORIZONTAL));
+        activityList.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
+        DividerItemDecoration decor = new DividerItemDecoration(this, DividerItemDecoration
+                .HORIZONTAL);
+        decor.setDrawable(AppCompatResources.getDrawable(this,R.drawable.bga_adapter_divider_shape));
+        activityList.addItemDecoration(decor);
         EnterListAdapter enterListAdapter = new EnterListAdapter(activityList, R.layout.recycler_chat);
         enterListAdapter.setData(data);
         activityList.setAdapter(enterListAdapter);
@@ -58,6 +72,35 @@ public class EnterListActivity extends BaseActivity {
                 Intent intent = new Intent();
                 intent.setClassName(getApplicationContext(), className);
                 startActivity(intent);
+            }
+        });
+
+        index.observe(this,offset->{
+            if (offset < activityList.getAdapter().getItemCount()) {
+                activityList.scrollToPosition(offset);
+            }
+        });
+
+        LinearSnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(activityList);
+        //监听 滚动 获取具体位置
+        activityList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    if (recyclerView != null && recyclerView.getChildCount() > 0) {
+                        try {
+                            // 获取居中的具体位置 时的角标
+                            RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                            View snapView = snapHelper.findSnapView(layoutManager);
+                            int  indexTmp = ((RecyclerView.LayoutParams) snapView.getLayoutParams()).getViewAdapterPosition();
+                            Log.d(TAG, "onScrollStateChanged" + " 滑动的 position ==" + index);
+                            index.setValue(indexTmp);
+                        } catch (Exception e) {
+                        }
+                    }
+                }
             }
         });
     }
